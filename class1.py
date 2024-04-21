1056f9064ffc55aaaacc1800c9f6e3b539eb6eaf
Abdul Basit <bbasitkdfbgvb@gmail.com>
2024-04-21 22:16:41 +0500

    Python_Class_01 Old Python Code Methods , New Python Code Methods , Str Data type , int Data type , Float Data type , Bool Data type , Tuple data type

---
+++ class1.py
@@ -0,0 +1,52 @@
+
+# old  python code methods
+
+name = "Abdul Basit "
+print(name)
+print(type(name))
+print(id(name))
+
+# new python code methods
+
+name1:str = '2'
+name3 =str(6)
+name = name1 + name3
+
+print(name)
+
+# str data type
+name4 : str = "Abdul Basit"
+print(type(name4))
+print(id(name4))
+print(name4)
+
+
+# int data type
+
+name5 : int = 2004
+print(type(name5))
+print(id(name5))
+print(name5)
+
+# Float data type
+
+name6 : float = 2.3
+print(type(name6))
+print(id(name6))
+print(name6)
+
+# Bool data type
+
+name_2 : bool = True
+print(name_2)
+print(type(name_2))
+print(id(name_2))
+
+# Tuple data type
+
+name7 : tuple[str,int,float] = ("Abdul Basit",2000 , 2.44)
+print(type(name7))
+print(id(name7))
+print(name7)
+
+
